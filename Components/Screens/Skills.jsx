483bb1951b2950b68ed62b4dd41d32778018f456
trble91 <daleyaaron@gmail.com>
2023-07-06 14:44:24 -0400

    addsb&w images

--- Components/Screens/Skills.jsx
+++ Components/Screens/Skills.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { motion } from "framer-motion"
+import { SocialIcon } from 'react-social-icons'
 
 export default function Skills() {
   return (
@@ -7,7 +8,12 @@ export default function Skills() {
     <motion.div className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center snap-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Skills
-        </h3>    
+        </h3>   
+        <div>
+          <div>
+          
+          </div>
+        </div> 
     </motion.div>
     </>
   )
