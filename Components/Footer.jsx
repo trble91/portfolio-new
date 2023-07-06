0b1144be7812946e198700180cafa6118dd6e453
trble91 <daleyaaron@gmail.com>
2023-07-06 18:16:02 -0400

    removes motion on footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -6,28 +6,15 @@ import Homepage from './Screens/Homepage';
 export default function footer() {
   return (
     <footer className="sticky bottom-0 p-5 flex-row-reverse items-end justify-between max-w-7xl mx-auto z-20 xl:items-center"> 
-        <motion.div 
-          initial={{
-            x: 500,
-            opacity: 0,
-            scale: 0.5,
-            }}
-          animate={{
-            x: 1,
-            opacity: 1,
-            scale: 1,
-            }}
-          transition={{
-            duration: 0.9,
-            }}
+        <div
             >
-            <a href="bit.ly/daleyportfolio" className='flex relative float-right'>
+            <a href="https://www.bit.ly/daleyportfolio" className='flex relative float-right'>
                 <Image 
                 src="/assets/aaron.png"
                 alt="" width={50} height={50}
                 />
            </a>
-        </motion.div>
+        </div>
     </footer>
   )
 }
