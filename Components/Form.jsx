3605e36123f40ede8e1b5e4b1ddd76b35d84e302
trble91 <daleyaaron@gmail.com>
2023-07-05 21:44:27 -0400

    matches colors

--- Components/Form.jsx
+++ Components/Form.jsx
@@ -4,15 +4,15 @@ export default function Form() {
   return (
     <form action="/api/form" method="post" className='text-gray-500 text-2xl sm:text-gray-500'>
       <label htmlFor="first">First Name</label>
-      <input type="text" id="first" name="first" required className='text-gray-100 bg-slate-400 rounded-lg'/>
+      <input type="text" id="first" name="first" required className='text-gray-100 bg-gray-400 rounded-lg uppercase'/>
  
       <label htmlFor="last" >Last Name</label>
-      <input type="text" id="last" name="last" required className='text-gray-100 bg-slate-400 rounded-lg'/>
+      <input type="text" id="last" name="last" required className='text-gray-100 bg-gray-400 rounded-lg uppercase'/>
       
       <label htmlFor="email" >Email</label>
-      <input type="email" id="email" name="email" required className='text-gray-100 bg-slate-400 rounded-lg'/>
-      <textarea className='text-gray-100 bg-slate-400 rounded-lg mt-5'/>
-      <button type="submit" className='text-gray-100 bg-slate-400 rounded-2xl mt-5 p-1 pr-2 pl-2'>Submit</button>
+      <input type="email" id="email" name="email" required className='text-gray-100 bg-gray-400 rounded-lg'/>
+      <textarea className='text-gray-100 bg-gray-400 rounded-lg mt-5'/>
+      <button type="submit" className='text-gray-100 bg-gray-400 rounded-2xl mt-5 p-1 pr-2 pl-2'>Submit</button>
     </form>
   )
 }
