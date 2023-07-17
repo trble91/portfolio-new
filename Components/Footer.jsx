57a5dd4f75f9f49601051a0e45c571264f345eb9
trble91 <daleyaaron@gmail.com>
2023-07-17 18:01:36 -0400

    adds new link on footer

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -5,7 +5,7 @@ export default function footer() {
   return (
     <footer className="sticky bottom-0 p-5 flex-row-reverse items-end justify-between max-w-7xl mx-auto z-20 xl:items-center"> 
         <div>
-            <a href="https://www.bit.ly/daleyportfolio" className='flex relative float-right'>
+            <a href="https://portfolio-new-trble91.vercel.app" className='flex relative float-right'>
                 <Image 
                 src="/assets/aaron.png"
                 alt="" width={50} height={50} />
