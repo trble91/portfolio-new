8ddcade23420e6500e8b8f2190a1a0d49feac460
trble91 <daleyaaron@gmail.com>
2023-12-06 18:47:01 -0500

    nav tailwind styling

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -9,16 +9,16 @@ export default function Nav() {
   return (
     <nav className="sticky bottom-0 p-5 flex-row-reverse items-end justify-between max-w-[600px] mx-auto z-20 xl:items-center"> 
      <Link href={<Experiences />}>
-      <a className=''>Experiences</a>
+      <a className='p-5'>Experiences</a>
      </Link>
      <Link href={<Photography />}>
-      <a className=''>Photography</a>
+      <a className='justify-between'>Photography</a>
      </Link>
      <Link href={<AboutMe />}>
-      <a className=''>Bio</a>
+      <a className='justify-between'>Bio</a>
      </Link>
      <Link href="/">
-      <a className=''>Home</a>
+      <a className='p-5'>Home</a>
      </Link>
     </nav>
   )
