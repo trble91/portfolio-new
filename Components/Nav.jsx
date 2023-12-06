c28ccdbde6dfaddd57445089a166a700282cf033
trble91 <daleyaaron@gmail.com>
2023-12-06 18:28:00 -0500

    fixes nav typo

--- Components/Nav.jsx
+++ Components/Nav.jsx
@@ -6,7 +6,7 @@ import Photography from './Screens/Photography';
 
 export default function Nav() {
   return (
-    <Nav className="sticky bottom-0 p-5 flex-row-reverse items-end justify-between max-w mx-auto z-20 xl:items-center"> 
+    <Nav className="sticky bottom-0 p-5 flex-row-reverse items-end justify-between max-w-[600px] mx-auto z-20 xl:items-center"> 
      <Link href={<Experiences />}>
       <a className=''>Experiences</a>
      </Link>
