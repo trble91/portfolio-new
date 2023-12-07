089ed9fae7bc08522229c604ca5f1058bb5b46a5
trble91 <daleyaaron@gmail.com>
2023-12-06 19:02:59 -0500

    uses older version of tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     purge: [
-      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./page/**/*.{js,ts,jsx,tsx,mdx}",
       "./components/**/*.{js,ts,jsx,tsx,mdx}",
       "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./screens/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     options: {
       safelist: [
