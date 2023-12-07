ee5fdd5c360a052a2bf42ac8e78674bf8c4ee2fc
trble91 <daleyaaron@gmail.com>
2023-12-07 17:49:33 -0500

    fixes layout. needs a navbar, dark mode text and updated skills list

--- app/page.js
+++ app/page.js
@@ -3,21 +3,23 @@ import React from "react";
 import Header from "@/Components/Header.jsx";
 import Homepage from "@/Components/Screens/Homepage.jsx";
 import AboutMe from "@/Components/Screens/AboutMe";
-import Skills from "@/Components/Screens/Skills";
+// import Skills from "@/Components/Screens/Skills";
 import Photography from "@/Components/Screens/Photography";
 // import Nav from "@/Components/Nav.jsx";
 import './globals.css'
+import Experiences from "@/Components/Screens/Experiences";
 
 export default function Home() {
   return (
     <>
       <React.StrictMode>
-        <div className="snap-mandatory min-h-screen">
+        <div className="snap-mandatory min-h-screen dark:bg-gray-800 dark:text-gray-300">
           <Header />
           {/* <Nav /> */}
           <Homepage />
           <AboutMe />
           {/* <Skills /> */}
+          <Experiences />
           <Photography />
         </div>
       </React.StrictMode>
