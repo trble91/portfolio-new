48ab0e2fe0001cbdc5992d533b99efc6e9d8004c
trble91 <daleyaaron@gmail.com>
2023-12-07 13:24:07 -0500

    hopefully she works

--- app/page.js
+++ app/page.js
@@ -2,7 +2,10 @@
 import React from "react";
 import Header from "@/Components/Header.jsx";
 import Homepage from "@/Components/Screens/Homepage.jsx";
-import Nav from "@/Components/Nav.jsx";
+import AboutMe from "@/Components/Screens/AboutMe";
+import Skills from "@/Components/Screens/Skills";
+import Photography from "@/Components/Screens/Photography";
+// import Nav from "@/Components/Nav.jsx";
 import './globals.css'
 
 export default function Home() {
@@ -11,8 +14,11 @@ export default function Home() {
       <React.StrictMode>
         <div className="snap-mandatory min-h-screen">
           <Header />
-          <Nav />
+          {/* <Nav /> */}
           <Homepage />
+          <AboutMe />
+          <Skills />
+          <Photography />
         </div>
       </React.StrictMode>
     </>
