2f9b708fb77632453ba1649cb2a6a64998d3ce71
trble91 <daleyaaron@gmail.com>
2023-12-15 18:01:33 -0500

    find icons for skills. too bland

--- Components/Skills.jsx
+++ Components/Skills.jsx
@@ -1,5 +1,7 @@
 
 import React from 'react'
+import { SocialIcon } from "react-social-icons"
+
 
 export default function Skills() {
   return (
@@ -8,8 +10,8 @@ export default function Skills() {
             Skills
         </h3>   
         <div className='flex absolute top-20 flex-row text-gray-700 text-md top-18 items-center max-w-600px px-10] dark:text-gray-900'>
-         <ul>
-          {/* <li>Javascript</li>
+       <ul>
+         <li>Javascript</li>
           <li>React</li>
           <li>Node.js</li>
           <li>Python</li>
@@ -25,103 +27,8 @@ export default function Skills() {
           <li>MongoDB</li>
           <li>Express</li>
           <li>PeeWee</li>
-          <li>Adobe Suite</li> */}
-          
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-<path d="M3 4v16h18V4zM3 4l9 8 9-8" />
-</svg>
-{/* <!-- Javascript --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M5 12h14M12 5l7 7-7 7" />
-</svg>
-{/* <!-- React --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M3 22h18M18 2a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3V2" />
-</svg>
-{/* <!-- Node.js --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M9 19l-7-7 7-7 7 7-7 7z" />
-</svg>
-{/* <!-- Python --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M20 2v16M4 2v16M10 14L4 2v12l6-10z" />
-</svg>
-{/* <!-- PostgreSQL --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM5 13h14M12 7v6" />
-</svg>
-{/* <!-- Tailwind --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <rect x="2" y="2" width="20" height="20" rx="2.18" ry="2.18" />
-  <line x1="7" y1="2" x2="7" y2="22" />
-  <line x1="17" y1="2" x2="17" y2="22" />
-  <line x1="2" y1="12" x2="22" y2="12" />
-</svg>
-{/* <!-- CSS --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M2 2h20v20H2z" />
-  <path d="M17 2l4 4-8 8H5v-8z" />
-</svg>
-{/* <!-- SCSS --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M2 2h20v20H2z" />
-  <path d="M17 2l4 4-8 8H5v-8z" />
-</svg>
-{/* <!-- HTML --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <rect x="2" y="2" width="20" height="20" rx="2" ry="2" />
-  <line x1="7" y1="2" x2="7" y2="22" />
-  <line x1="17" y1="2" x2="17" y2="22" />
-  <line x1="2" y1="12" x2="22" y2="12" />
-</svg>
-{/* <!-- Framer-Motion --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M21.333 21.333l-9.693-9.693c-2.91-2.91-3.47-7.48-1.613-10.827a9.326 9.326 0 0 1 12.938-3.296 9.326 9.326 0 0 1 3.296 12.938c-3.348 1.857-7.917 1.297-10.827-1.613l-9.693-9.693" />
-</svg>
-{/* <!-- Flask --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M4 16h6v6H4zM4 2h6v6H4zM14 14h6v6h-6zM14 2h6v6h-6z" />
-</svg>
-{/* <!-- Django --> */}
-
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2">
-</svg>
-
-{/* <!-- MongoDB --> */}
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-3.08 14.25c.6 0 1.11-.17 1.56-.5l8.22-8.22c.34-.34.56-.78.62-1.25s-.04-.91-.38-1.25-.79-.56-1.25-.62c-.47-.06-.92.04-1.25.38l-7.3 7.3-3.47-3.47c-.68-.68-1.79-.68-2.47 0s-.68 1.79 0 2.47l4.47 4.47c.44.44 1.02.66 1.59.66z"/>
-</svg>
-
-{/* <!-- Express --> */}
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M1.75 1.75v7.5l6.364 2.727L13.5 9.5v-7.5zM13.5 16.5l6.73-2.272L22.25 8.25H13.5v8.25zM22.25 15.75L15.5 13.5V0H6.75L0 2.25l6.364 2.727 6.364 2.727-6.364 2.727L0 12v2.25h3.568l5.682 3.125L15.5 15v8.25h6.75l6.75-2.25v-7.5h-3.568zM10.134 13.875L3.318 16.28l3.584 1.925L13.5 14.125v-2.25l-7.366 2.25z"/>
-</svg>
-
-{/* <!-- PeeWee --> */}
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M6 8l-4 8h16l-4-8H6zm0 16h12m-12-2h4m-4-4h4m-4-4h12"/>
-</svg>
-
-{/* <!-- Adobe Suite --> */}
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <rect x="2" y="2" width="20" height="20" rx="2.18" ry="2.18" />
-  <path d="M16 2l-6 6-4-4" />
-  <path d="M15 9l6 6M9 15l-6 6" />
-</svg>
-
-
-         </ul>
+          <li>Adobe Suite</li> 
+          </ul>
         </div>
     </div>
   )
