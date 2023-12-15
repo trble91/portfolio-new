cd93c70a2b0604407fbb748d4360ec23409b30e3
trble91 <daleyaaron@gmail.com>
2023-12-15 17:38:36 -0500

    adds clients to components for rendering

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React from "react";
+import React from 'react';
 import Header from "@/components/Header.jsx";
 import Homepage from "@/app/Home/page.jsx";
 import Navbar from "@/components/Navbar.jsx";
