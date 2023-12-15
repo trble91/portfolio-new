633e8e36d11b0053fa0d3dc1f19893eb3b9c93c1
trble91 <daleyaaron@gmail.com>
2023-12-15 17:50:50 -0500

    adds svg

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React from "react";
 import { motion } from "framer-motion";
 
@@ -22,13 +22,13 @@ export default function Homepage() {
         className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[200px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center snap-end"
       >
         <h3 className="absolute top-24 uppercase tracking-[17px] text-gray-700 text-2xl sm:text-gray-700">
-          The way to get started is to {'\n'} 
-          quit talking and begin doing 
+          The way to get started is to {"\n"}
+          quit talking and begin doing
         </h3>
         <p className="absolute bottom-70 uppercase tracking-[2px] text-gray-700 text-md marker:mb-10">
           - Walt Disney
         </p>
       </motion.div>
-      </>
+    </>
   );
 }
--- app/Photography/page.jsx
+++ app/Photography/page.jsx
@@ -1,59 +1,80 @@
-"use client"
-import React from 'react'
-import { motion } from "framer-motion"
-import Image from 'next/image';
-import Navbar from '@/components/Navbar';
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
 
 export default function Photography() {
   return (
     <>
-   <div className='flex sticky flex-row text-center sm:max-w-[300px]  md:flex-row md:text-left xl:flex-row max-w-[400px] xl:px-5 min-h-screen justify-center xl: space-y-4 mx-auto items-center snap-center'>
-    <h3 className='absolute top-10 uppercase tracking-[17px] text-gray-700 text-2xl sm:text-gray-700 sm:text-base md:text-gray-700 lg:text-gray-700 xl:text-gray-800'>
-        Photography
-    </h3>   
-    <>
-        <motion.div 
-         initial={{
-             x: -500,
-             opacity: 0,
-             scale: 0.5,
+      <div className="flex sticky flex-row text-center sm:max-w-[300px]  md:flex-row md:text-left xl:flex-row max-w-[400px] xl:px-5 min-h-screen justify-center xl: space-y-4 mx-auto items-center snap-center">
+        <h3 className="absolute top-10 uppercase tracking-[17px] text-gray-700 text-2xl sm:text-gray-700 sm:text-base md:text-gray-700 lg:text-gray-700 xl:text-gray-800">
+          Photography
+        </h3>
+        <>
+          <motion.div
+            initial={{
+              x: -500,
+              opacity: 0,
+              scale: 0.5,
             }}
             animate={{
-                x: 0,
-                opacity: 1,
-                scale: 1,
+              x: 0,
+              opacity: 1,
+              scale: 1,
             }}
             transition={{
-                duration: 3.0,
+              duration: 3.0,
             }}
-            className='flex relative flex-col text-center mt-20 snap-center'>
-        <Image src="/assets/bride.jpg" alt="Bride" width={200} height={50} />
-        <Image src="/assets/yankee Large.jpeg" alt="Yankee Stadium" width={200} height={50} />
-        </motion.div>
-    </>
-    <>
-        <motion.div 
-         initial={{
-             x: 500,
-             opacity: 0,
-             scale: 0.5,
+            className="flex relative flex-col text-center mt-20 snap-center"
+          >
+            <Image
+              src="/assets/bride.jpg"
+              alt="Bride"
+              width={200}
+              height={50}
+            />
+            <Image
+              src="/assets/yankee Large.jpeg"
+              alt="Yankee Stadium"
+              width={200}
+              height={50}
+            />
+          </motion.div>
+        </>
+        <>
+          <motion.div
+            initial={{
+              x: 500,
+              opacity: 0,
+              scale: 0.5,
             }}
             animate={{
-                x: 0,
-                opacity: 1,
-                scale: 1,
+              x: 0,
+              opacity: 1,
+              scale: 1,
             }}
             transition={{
-                duration: 3.0,
-            }} 
-            className='flex relative flex-col text-center mt-20 snap-center'>
-        <Image src="/assets/groom Large.jpeg" alt="groom" width={200} height={50} />
-        <Image src="/assets/corner Large.jpeg" alt="cornerClass" width={200} height={50} />
-        </motion.div>
+              duration: 3.0,
+            }}
+            className="flex relative flex-col text-center mt-20 snap-center"
+          >
+            <Image
+              src="/assets/groom Large.jpeg"
+              alt="groom"
+              width={200}
+              height={50}
+            />
+            <Image
+              src="/assets/corner Large.jpeg"
+              alt="cornerClass"
+              width={200}
+              height={50}
+            />
+          </motion.div>
+        </>
+      </div>
+      <Navbar />
     </>
-    </div>
-    <Navbar />
-    </>
-  )
+  );
 }
-
