9988434fbc2f95f6a7f0ad54fb509cb7ec73bc19
trble91 <daleyaaron@gmail.com>
2023-12-24 12:40:12 -0500

    fixes button

---
+++ Components/Footer.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+    <div>Footer</div>
+    </>
+  )
+}
+
--- app/Experiences/page.jsx
+++ app/Experiences/page.jsx
@@ -79,7 +79,7 @@ export default function Experiences() {
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{slides[currentSlide].description}</p>
                 <a
                   href={slides[currentSlide].link}
-                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-300 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-300 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-blue-800"
                 >
                   Read more
                   <svg
