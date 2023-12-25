62d6212262fb7e13c8e5964f1068a65193c4e1f4
trble91 <daleyaaron@gmail.com>
2023-12-24 20:53:34 -0500

    arrow

--- app/Experiences/page.jsx
+++ app/Experiences/page.jsx
@@ -89,6 +89,7 @@ export default function Experiences() {
                     fill="none"
                     viewBox="0 0 14 10"
                   >
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />                  
                   </svg>
                 </a>
               </div>
