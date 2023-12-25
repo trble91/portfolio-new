a3333fe97fd4324d8ab76827801e669be4eed410
trble91 <daleyaaron@gmail.com>
2023-12-24 21:44:44 -0500

    styles

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 "use client"
 
-import './styles.css'
+import "./styles.css"
 import React from 'react';
 import Header from "@/components/Header.jsx";
 import Homepage from "@/app/Home/page.jsx";
 import Navbar from "@/components/Navbar.jsx";
+
 
 export default function Home() {
 
