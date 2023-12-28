85d7de6d013be72ef7d0b117dd8a8a2ede1439b5
trble91 <daleyaaron@gmail.com>
2023-12-28 16:16:53 -0500

    removes tab

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <nav className="relative absolute space-x-4 space-y-8 text-center p-5 uppercase mb-2 -mt-10 tracking-[10px] text-gray-700">
       <Link href="/">Home</Link>
-      <Link href="/Contact">Contact</Link>
+      {/* <Link href="/Contact">Contact</Link> */}
       <Link href="/Experiences">Experiences</Link>
       <Link href="/Photography">Photography</Link>
       <Link href="/Skills">Skills</Link>
