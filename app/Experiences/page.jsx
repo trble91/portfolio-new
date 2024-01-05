e57f87a3f7fbdef606b0a0efb94f4ad2609f9586
trble91 <daleyaaron@gmail.com>
2024-01-05 08:12:04 -0500

    removes lines

--- app/Experiences/page.jsx
+++ app/Experiences/page.jsx
@@ -96,7 +96,7 @@ export default function Experiences() {
                   href={slides[currentSlide].link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-100 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-blue-800"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-300 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-blue-800"
                 >
                   Read more
                   <svg
@@ -106,13 +106,6 @@ export default function Experiences() {
                     fill="none"
                     viewBox="0 0 14 10"
                   >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M1 5h12m0 0L9 1m4 4L9 9"
-                    />
                   </svg>
                 </a>
               </div>
