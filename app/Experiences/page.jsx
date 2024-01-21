6fa79c9f2cea8c1d0a7971d65691571a7589a203
trble91 <daleyaaron@gmail.com>
2024-01-21 12:23:40 -0500

    fixes button and nav text

--- app/Experiences/page.jsx
+++ app/Experiences/page.jsx
@@ -84,18 +84,18 @@ export default function Experiences() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">
                     {slides[currentSlide].title}
                   </h5>
                 </a>
-                <p className="mb-3 font-normal text-white dark:text-gray-200">
+                <p className="mb-3 font-normal text-white">
                   {slides[currentSlide].description}
                 </p>
                 <a
                   href={slides[currentSlide].link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-600 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-blue-800"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-300 rounded-lg hover:bg-gray-600 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-blue-800"
                 >
                   Read more
                   <svg
@@ -115,6 +115,7 @@ export default function Experiences() {
                 </a>
               </div>
             </div>
+                    <p><b>Loading...</b></p>
           </div>
         </motion.div>
       </div>
--- app/Navbar/page.jsx
+++ app/Navbar/page.jsx
@@ -5,11 +5,11 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="relative space-x-4 space-y-8 absolute text-center p-5 uppercase mb-2 -mt-10 tracking-[10px]">
+    <nav className="relative space-x-4 space-y-8 text-center p-5 uppercase mb-2 -mt-10 tracking-[10px]">
       <Link href="/">Home</Link>
       {/* <Link href="/Contact">Contact</Link> */}
-      <Link href="/Experiences">Experiences</Link>
-      <Link href="/Photography">Photography</Link>
+      <Link href="/Experiences">Projects</Link>
+      <Link href="/Photography">Photo</Link>
       <Link href="/Skills">Skills</Link>
     </nav>
   );
